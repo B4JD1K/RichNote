16176c854ba01c8b8bc15968cc9ae5473c5cd1db
B4JD1K <bajdik@yahoo.com>
2025-09-18 23:54:53 +0200

    feat(ui, components): add reusable sidebar, breadcrumb, header, and form components

---
+++ components/ui/forms/login-form_cp.tsx
@@ -0,0 +1,137 @@
+"use client"
+
+import {z} from "zod"
+import {cn} from "@/lib/utils"
+import {Button} from "@/components/ui/button"
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card"
+import {Input} from "@/components/ui/input"
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {Form, FormControl, FormField, FormItem, FormMessage} from "@/components/ui/form"
+import {signInUser} from "@/server/users";
+import {ComponentProps, useState} from "react";
+import {toast} from "sonner";
+import {Loader2} from "lucide-react";
+import Link from "next/link";
+import {useRouter} from "next/navigation";
+
+const formSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8),
+})
+
+export function LoginForm({className, ...props}: ComponentProps<"div">) {
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    },
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      setIsLoading(true)
+      const res = await signInUser(
+        values.email,
+        values.password
+      )
+
+      if (res.success) {
+        toast.success(res.message)
+        router.push("/dashboard")
+      } else toast.error(res.message)
+    } catch (e) {
+      console.log(e)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Login to your account
+          </CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-3">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({field}) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input placeholder="your@email.com" {...field} />
+                        </FormControl>
+                        <FormMessage/>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({field}) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="Password"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage/>
+                        <Link
+                          href="#"
+                          className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                        >
+                          Forgot your password?
+                        </Link>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="flex flex-col gap-3">
+                  <Button
+                    type="submit"
+                    className="w-full"
+                    disabled={isLoading}
+                  >
+                    {isLoading ? <Loader2 className="size-4 animate-spin"/> : "Login"}
+                  </Button>
+                  <div className="flex gap-3 justify-between">
+                    <Button variant="outline" className="max-w-1/2 min-w-49">
+                      Login with Google
+                    </Button>
+                    <Button variant="outline" className="max-w-1/2 min-w-49">
+                      Login with GitHub
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              {/*<div className="mt-4 text-center text-sm">*/}
+              {/*  Don&apos;t have an account?{" "}*/}
+              {/*  <Link href="/register" className="underline underline-offset-4">*/}
+              {/*    Log in!*/}
+              {/*  </Link>*/}
+              {/*</div>*/}
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
